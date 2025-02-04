af22e2e58d802559668bc22b549d77269d4e724e
yasithppasindu <164892806+yasithh1@users.noreply.github.com>
2025-02-04 10:06:39 +0530

    upper body ui desing updates

--- app/src/main/java/com/example/fitnesstracker/UpperBodyWorkoutActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/UpperBodyWorkoutActivity.kt
@@ -1,17 +1,48 @@
 package com.example.fitnesstracker
 
-
+import android.animation.Animator
+import android.animation.AnimatorInflater
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class UpperBodyWorkoutActivity  : AppCompatActivity() {
+class UpperBodyWorkoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.upperbodyworkout_activity)
+    }
 
+    // Click handler for card views
+    fun onCardClick(view: View) {
+        val scaleUp = AnimatorInflater.loadAnimator(this, R.animator.scale_up) as AnimatorSet
+        val scaleDown = AnimatorInflater.loadAnimator(this, R.animator.scale_down) as AnimatorSet
+        scaleUp.setTarget(view)
+        scaleDown.setTarget(view)
 
+        scaleUp.start()
+        scaleUp.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                scaleDown.start()
+            }
+        })
+
+        Toast.makeText(this, "Card clicked!", Toast.LENGTH_SHORT).show()
+    }
+
+    // Click handler for the "Start Workout" button
+    fun onStartWorkoutClick(view: View) {
+        val fadeIn = AnimatorInflater.loadAnimator(this, R.animator.fade_in) as AnimatorSet
+        fadeIn.setTarget(view)
+        fadeIn.start()
+
+        // Additional logic for starting the workout can be added here
+        Toast.makeText(this, "Workout Started!", Toast.LENGTH_SHORT).show()
     }
 }
